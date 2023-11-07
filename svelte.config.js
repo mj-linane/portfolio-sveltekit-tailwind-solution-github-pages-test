9ae9c4f013301a7328502541b9655e284e246b1d
mj-linane <19750620+mj-linane@users.noreply.github.com>
2023-11-07 13:20:37 -0500

    Update Svelte adapter and Vite base configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: null
+    }),
   },
+  vite: {
+    base: '/portfolio-sveltekit-tailwind-solution-github-pages-test/', // replace 'your-repo-name' with the name of your GitHub repository
+  }
 };
 
 export default config;
